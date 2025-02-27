aa06a491a77208bebfba2dcc4959753d1645f285
kingle1024 <teran1024@naver.com>
2025-02-27 20:49:50 +0900

    ✨ Feat: 회사 버튼 잘보이도록 처리

--- src/components/ListContainer.tsx
+++ src/components/ListContainer.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios, { AxiosResponse } from 'axios';
 import API_URL from "../config";
 import './ListContainer.css';
+import { IonButton } from '@ionic/react';
 
 interface Job_mst {
   id: number | null;
@@ -40,14 +41,33 @@ const ListContainer: React.FC = () => {
     setCompany(selectedCompany);
   };
 
+  const companies:any = {
+    NAVER: '네이버',
+    KAKAO: '카카오',
+    LINE: '라인',
+    COUPANG: '쿠팡',
+    BAEMIN: '배달의민족', 
+    DAANGN: '당근마켓',
+    TOSS: '토스',
+    YANOLJA: '야놀자',
+  };
+
   return (
     <div className="container">
       <div className="announcement-section">
         <h1>공고 목록</h1>
         
-        <div className="button-section">
-          <button onClick={() => handleCompanyChange('NAVER')}>네이버</button>
-          <button onClick={() => handleCompanyChange('LINE')}>라인</button>
+        <div className="button-section">         
+          {Object.keys(companies).map((comp) => (
+            <IonButton 
+              key={comp}
+              expand="full" 
+              onClick={() => handleCompanyChange(comp)} 
+              color={company === comp ? 'primary' : 'medium'}
+            >
+              {companies[comp]} {/* 매핑된 회사 이름 사용 */}
+            </IonButton>
+          ))}
         </div>
 
         <ul>
@@ -62,7 +82,6 @@ const ListContainer: React.FC = () => {
           )}
         </ul>
       </div>
-      
     </div>
   );
 };
