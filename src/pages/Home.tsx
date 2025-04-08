6df6fe7ea103e15223b59ea9c3561455355bf6e0
kingle1024 <teran1024@naver.com>
2025-04-08 11:47:03 +0900

    🚨 Fix: 메타태그 변경

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -99,9 +99,14 @@ const Home: React.FC = () => {
   return (
     <IonPage>
       <Helmet>
-        <title>네카라쿠배당토야 공고</title>
-        <meta name="description" content="네카라쿠배당토야 공고 페이지입니다." />
-        <meta name="keywords" content="네카라쿠배당토야 채용 공고" />
+        <title>네카라쿠배 공고</title>
+        <meta name="description" content="네카라쿠배 공고 페이지" />
+        <meta name="keywords" content="네카라쿠배 채용, 네이버 채용, 카카오 채용, 라인 채용, 쿠팡 채용, 우아한형제들 채용, 당근 채용,토스 채용, 
+  프론트엔드 채용, 백엔드 채용, 풀스택 개발자 채용, 모바일 개발자 채용, UI/UX 디자이너 채용,
+  그래픽 디자이너 채용, 데이터 분석가 채용, AI 엔지니어 채용, 머신러닝 엔지니어 채용, 서비스 기획 채용,
+  프로덕트 매니저 채용, 프로젝트 매니저 채용, 신입 공개 채용, 경력직 공개 채용, 네카라쿠배 수시 채용, IT 기업 수시 채용,
+  네카라쿠배 인턴 채용, IT 기업 인턴십, 원격 근무 채용, 재택 근무 가능 채용, 2025 개발자 채용, IT 채용 트렌드, 네카라쿠배 채용 공고,
+  네카라쿠배 공고, 블록체인 개발자 채용, AI 스타트업 채용, IT 대기업 채용" />
         <meta name="author" content="네카라쿠배당토야 팀" />
       </Helmet>
       <IonHeader>
